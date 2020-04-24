07509ffe9469d4edd1f2cb0f8008217b92fe240b
Jose Castro <drakonc@gmail.com>
2020-04-23 22:12:05 -0500

    Server Ping

--- ServPing.js
+++ ServPing.js
@@ -1,7 +1,9 @@
 var ping = require('ping');
 
-var hosts = ['192.168.200.37', '192.168.200.39', '192.168.200.10', '192.168.200.50', '192.168.200.4',
-    '192.168.200.40', '192.168.200.36'];
+var hosts = [
+    '192.168.200.4', '192.168.200.10', '192.168.200.18', '192.168.200.21', '192.168.200.37',
+    '192.168.200.39', '192.168.200.40', '192.168.200.50'
+];
 
 async function isPing() {
     for (let host of hosts) {
@@ -9,6 +11,7 @@ async function isPing() {
         let isAlive = res.alive
         var msg = isAlive ? 'host ' + res.host + ' time = ' + res.time + ' is ' + isAlive : 'host ' + host + ' time = ' + res.time + ' is ' + isAlive;
         console.log(msg);
+
     }
 }
 console.log('Realizando Ping...')
